25983434a84feaefa5c7061b5319b5a2c7a26ab5
Vinayak Mehta <vmehta94@gmail.com>
2016-08-31 17:52:28 +0530

    Make new module

--- camelot/lattice.py
+++ camelot/lattice.py
@@ -4,11 +4,10 @@ import types
 import copy_reg
 import logging
 
-import cv2
-import numpy as np
-
 from wand.image import Image
 
+from .image_ops import (adaptive_threshold, extract_lines, extract_table_contours,
+                        extract_table_joints)
 from .table import Table
 from .utils import (transform, segments_bbox, text_bbox, detect_vertical, merge_close_values,
                     get_row_index, get_column_index, get_score, reduce_index,
@@ -24,94 +23,6 @@ def _reduce_method(m):
     else:
         return getattr, (m.im_self, m.im_func.func_name)
 copy_reg.pickle(types.MethodType, _reduce_method)
-
-
-def _adaptive_threshold(imagename, invert=False):
-    img = cv2.imread(imagename)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    if invert:
-        threshold = cv2.adaptiveThreshold(
-            gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
-            15, -0.2)
-    else:
-        threshold = cv2.adaptiveThreshold(
-            np.invert(gray), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-            cv2.THRESH_BINARY,
-            15, -0.2)
-    return img, threshold
-
-
-def _extract_lines(threshold, direction=None, scale=15):
-    lines = []
-
-    if direction == 'vertical':
-        size = threshold.shape[0] // scale
-        el = cv2.getStructuringElement(cv2.MORPH_RECT, (1, size))
-    elif direction == 'horizontal':
-        size = threshold.shape[1] // scale
-        el = cv2.getStructuringElement(cv2.MORPH_RECT, (size, 1))
-    elif direction is None:
-        raise ValueError("Specify direction as either 'vertical' or"
-                         " 'horizontal'")
-
-    threshold = cv2.erode(threshold, el, (-1, -1))
-    threshold = cv2.dilate(threshold, el, (-1, -1))
-
-    dmask = threshold  # findContours modifies source image
-
-    try:
-        _, contours, _ = cv2.findContours(
-            threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    except ValueError:
-        contours, _ = cv2.findContours(
-            threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
-        x1, x2 = x, x + w
-        y1, y2 = y, y + h
-        if direction == 'vertical':
-            lines.append(((x1 + x2) / 2, y2, (x1 + x2) / 2, y1))
-        elif direction == 'horizontal':
-            lines.append((x1, (y1 + y2) / 2, x2, (y1 + y2) / 2))
-
-    return dmask, lines
-
-
-def _extract_table_contours(vertical, horizontal):
-    mask = vertical + horizontal
-    joints = np.bitwise_and(vertical, horizontal)
-
-    try:
-        __, contours, __ = cv2.findContours(
-            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    except ValueError:
-        contours, __ = cv2.findContours(
-            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
-
-    tables = {}
-    for c in contours:
-        c_poly = cv2.approxPolyDP(c, 3, True)
-        x, y, w, h = cv2.boundingRect(c_poly)
-        roi = joints[y : y + h, x : x + w]
-        try:
-            __, jc, __ = cv2.findContours(
-                roi, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        except ValueError:
-            jc, __ = cv2.findContours(
-                roi, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        if len(jc) <= 4:  # remove contours with less than 4 joints
-            continue
-        joint_coords = []
-        for j in jc:
-            jx, jy, jw, jh = cv2.boundingRect(j)
-            c1, c2 = x + (2 * jx + jw) / 2, y + (2 * jy + jh) / 2
-            joint_coords.append((c1, c2))
-        tables[(x, y + h, x + w, y)] = joint_coords
-
-    return tables
 
 
 class Lattice:
@@ -188,12 +99,13 @@ class Lattice:
             png.save(filename=imagename)
         pdf_x = width
         pdf_y = height
-        img, threshold = _adaptive_threshold(imagename, invert=self.invert)
-        vmask, v_segments = _extract_lines(threshold, direction='vertical',
+        img, threshold = adaptive_threshold(imagename, invert=self.invert)
+        vmask, v_segments = extract_lines(threshold, direction='vertical',
             scale=self.scale)
-        hmask, h_segments = _extract_lines(threshold, direction='horizontal',
+        hmask, h_segments = extract_lines(threshold, direction='horizontal',
             scale=self.scale)
-        table_bbox = _extract_table_contours(vmask, hmask)
+        contours = extract_table_contours(vmask, hmask)
+        table_bbox = extract_table_joints(contours, vmask, hmask)
         img_x = img.shape[1]
         img_y = img.shape[0]
         scaling_factor_x = pdf_x / float(img_x)
