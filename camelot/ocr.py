7dc1d986115cfd1facb0160d965f25d7ba0c3887
Vinayak Mehta <vmehta94@gmail.com>
2016-09-17 14:40:19 +0530

    Fix coordinates

--- camelot/ocr.py
+++ camelot/ocr.py
@@ -13,12 +13,13 @@ from .utils import merge_close_values, encode_list
 class OCR:
     """OCR
     """
-    def __init__(self, dpi=300, lang="eng", scale=15, mtol=2):
+    def __init__(self, dpi=300, lang="eng", scale=15, mtol=2, debug=False):
         self.tool = pyocr.get_available_tools()[0]
         self.dpi = dpi
         self.lang = lang
         self.scale = scale
         self.mtol = mtol
+        self.debug = debug
 
     def get_tables(self, pdfname):
         if self.tool is None:
@@ -47,22 +48,20 @@ class OCR:
         page = {}
         tables = {}
         table_no = 0
-        for k in sorted(table_bbox.keys(), key=lambda x: x[1], reverse=True):
+        for k in sorted(table_bbox.keys(), key=lambda x: x[1]):
+            table_data = {}
             cols, rows = zip(*table_bbox[k])
             cols, rows = list(cols), list(rows)
             cols.extend([k[0], k[2]])
             rows.extend([k[1], k[3]])
             cols = merge_close_values(sorted(cols), mtol=self.mtol)
-            rows = merge_close_values(
-                sorted(rows, reverse=True), mtol=self.mtol)
+            rows = merge_close_values(sorted(rows, reverse=True), mtol=self.mtol)
             cols = [(cols[i], cols[i + 1])
                     for i in range(0, len(cols) - 1)]
             rows = [(rows[i], rows[i + 1])
                     for i in range(0, len(rows) - 1)]
             table = Table(cols, rows)
             table.image = img[k[3]:k[1],k[0]:k[2]]
-
-            cell_no = 0
             for i in range(len(table.cells)):
                 for j in range(len(table.cells[i])):
                     x1 = int(table.cells[i][j].x1)
@@ -70,17 +69,14 @@ class OCR:
                     x2 = int(table.cells[i][j].x2)
                     y2 = int(table.cells[i][j].y2)
                     table.cells[i][j].image = img[y1:y2,x1:x2]
-                    cell_no += 1
-
-            for i in range(len(table.cells)):
-                for j in range(len(table.cells[i])):
-                    text = tool.image_to_string(
+                    text = self.tool.image_to_string(
                         Image.fromarray(table.cells[i][j].image),
                         lang=self.lang,
                         builder=pyocr.builders.TextBuilder()
                     )
                     table.cells[i][j].add_text(text)
             ar = table.get_list()
+            ar.reverse()
             ar = encode_list(ar)
             table_data['data'] = ar
             tables['table-{0}'.format(table_no + 1)] = table_data
