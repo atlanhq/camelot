02ef332bd63df68e447ff671b1f1e12ad1f8f4c2
Vinayak Mehta <vmehta94@gmail.com>
2016-10-04 20:59:52 +0530

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -153,13 +153,13 @@ html_theme_options = {
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = None
+html_logo = 'assets/camelot.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-# html_favicon = None
+html_favicon = 'assets/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
