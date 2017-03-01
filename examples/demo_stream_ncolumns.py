3651fb234718ba3b02a3de743071f757ce936ed0
Vinayak Mehta <vmehta94@gmail.com>
2017-03-01 19:53:48 +0530

    Remove ncolumns everywhere

--- examples/demo_stream_ncolumns.py
+++
@@ -1,8 +0,0 @@
-from camelot import Pdf
-from camelot import Stream
-
-
-extractor = Stream(Pdf("files/missing_values.pdf",
-                       char_margin=1.0, clean=True), ncolumns=5)
-tables = extractor.get_tables()
-print tables
