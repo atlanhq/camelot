9c4e7bf3f6574c618028541653ce9fcfd54dabb3
Vinayak Mehta <vmehta94@gmail.com>
2018-09-24 21:08:10 +0530

    Add long description content type

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ def setup_package():
                     version=about['__version__'],
                     description=about['__description__'],
                     long_description=readme,
+                    long_description_content_type="text/markdown",
                     url=about['__url__'],
                     author=about['__author__'],
                     author_email=about['__author_email__'],
