eed883b1019564940ae8da97e8764d43733aaacd
Vinayak Mehta <vmehta94@gmail.com>
2018-09-28 19:37:59 +0530

    Update .gitignore again

--- .eggs/pdfminer.six-20170720-py2.7.egg/pdfminer/settings.py
+++
@@ -1,8 +0,0 @@
-STRICT = False
-
-try:
-    from django.conf import settings
-    STRICT = getattr(settings, 'PDF_MINER_IS_STRICT', STRICT)
-except Exception:
-    # in case it's not a django project
-    pass
