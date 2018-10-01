3b07cfb9397b71d63657ad1ae24059b4ca54d35b
Vinayak Mehta <vmehta94@gmail.com>
2018-10-01 23:56:53 +0530

    Remove unused utils

--- camelot/utils.py
+++ camelot/utils.py
@@ -136,35 +136,6 @@ def scale(x, s):
     return x
 
 
-def rotate(x1, y1, x2, y2, angle):
-    """Rotates point x2, y2 about point x1, y1 by angle.
-
-    Parameters
-    ----------
-    x1 : float
-    y1 : float
-    x2 : float
-    y2 : float
-    angle : float
-        Angle in radians.
-
-    Returns
-    -------
-    xnew : float
-    ynew : float
-
-    """
-    s = np.sin(angle)
-    c = np.cos(angle)
-    x2 = translate(-x1, x2)
-    y2 = translate(-y1, y2)
-    xnew = c * x2 - s * y2
-    ynew = s * x2 + c * y2
-    xnew = translate(x1, xnew)
-    ynew = translate(y1, ynew)
-    return xnew, ynew
-
-
 def scale_pdf(k, factors):
     """Translates and scales pdf coordinate space to image
     coordinate space.
@@ -339,33 +310,6 @@ def text_in_bbox(bbox, text):
                 <= rt[0] + 2 and lb[1] - 2 <= (t.y0 + t.y1) / 2.0
                 <= rt[1] + 2]
     return t_bbox
-
-
-def remove_close_lines(ar, line_close_tol=2):
-    """Removes lines which are within a tolerance, based on their x or
-    y axis projections.
-
-    Parameters
-    ----------
-    ar : list
-    line_close_tol : int, optional (default: 2)
-
-    Returns
-    -------
-    ret : list
-
-    """
-    ret = []
-    for a in ar:
-        if not ret:
-            ret.append(a)
-        else:
-            temp = ret[-1]
-            if np.isclose(temp, a, atol=line_close_tol):
-                pass
-            else:
-                ret.append(a)
-    return ret
 
 
 def merge_close_lines(ar, line_close_tol=2):
@@ -644,27 +588,6 @@ def compute_whitespace(d):
     return whitespace
 
 
-def remove_empty(d):
-    """Removes empty rows and columns from a two-dimensional list.
-
-    Parameters
-    ----------
-    d : list
-
-    Returns
-    -------
-    d : list
-
-    """
-    for i, row in enumerate(d):
-        if row == [''] * len(row):
-            d.pop(i)
-    d = zip(*d)
-    d = [list(row) for row in d if any(row)]
-    d = zip(*d)
-    return d
-
-
 def get_page_layout(filename, char_margin=1.0, line_margin=0.5, word_margin=0.1,
                detect_vertical=True, all_texts=True):
     """Returns a PDFMiner LTPage object and page dimension of a single
@@ -747,27 +670,3 @@ def get_text_objects(layout, ltype="char", t=None):
     except AttributeError:
         pass
     return t
-
-
-def merge_tuples(tuples):
-    """Merges a list of overlapping tuples.
-
-    Parameters
-    ----------
-    tuples : list
-        List of tuples where a tuple is a single axis coordinate pair.
-
-    Yields
-    ------
-    tuple
-
-    """
-    merged = list(tuples[0])
-    for s, e in tuples:
-        if s <= merged[1]:
-            merged[1] = max(merged[1], e)
-        else:
-            yield tuple(merged)
-            merged[0] = s
-            merged[1] = e
-    yield tuple(merged)
