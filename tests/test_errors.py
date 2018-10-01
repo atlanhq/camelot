7f51dbedb33c5f87942b33bad3b6003ef45237d3
Vinayak Mehta <vmehta94@gmail.com>
2018-10-01 23:37:29 +0530

    Add input kwargs test

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -9,11 +9,17 @@ import camelot
 
 testdir = os.path.dirname(os.path.abspath(__file__))
 testdir = os.path.join(testdir, "files")
+filename = os.path.join(testdir, 'foo.pdf')
 
 
 def test_unknown_flavor():
     message = ("Unknown flavor specified."
                " Use either 'lattice' or 'stream'")
     with pytest.raises(NotImplementedError, message=message):
-        filename = os.path.join(testdir, "assam.pdf")
-        tables = camelot.read_pdf(filename, flavor="chocolate")
+        tables = camelot.read_pdf(filename, flavor='chocolate')
+
+
+def test_input_kwargs():
+    message = "columns cannot be used with flavor='lattice'"
+    with pytest.raises(ValueError, message=message):
+        tables = camelot.read_pdf(filename, columns=['10,20,30,40'])
