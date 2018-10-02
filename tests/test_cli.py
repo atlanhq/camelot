8fa5e94e60fa7363fdf3eb5f93b772d2d8ea8b12
Vinayak Mehta <vmehta94@gmail.com>
2018-10-03 01:03:44 +0530

    Add CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+
+import os
+
+from click.testing import CliRunner
+
+from camelot.cli import cli
+from camelot.utils import TemporaryDirectory
+
+
+testdir = os.path.dirname(os.path.abspath(__file__))
+testdir = os.path.join(testdir, 'files')
+
+
+def test_cli_lattice():
+    with TemporaryDirectory() as tempdir:
+        infile = os.path.join(testdir, 'foo.pdf')
+        outfile = os.path.join(tempdir, 'foo.csv')
+        runner = CliRunner()
+        result = runner.invoke(cli, ['--format', 'csv', '--output', outfile,
+                                     'lattice', infile])
+        assert result.exit_code == 0
+        assert result.output == 'Found 1 tables\n'
+
+
+def test_cli_stream():
+    with TemporaryDirectory() as tempdir:
+        infile = os.path.join(testdir, 'budget.pdf')
+        outfile = os.path.join(tempdir, 'budget.csv')
+        runner = CliRunner()
+        result = runner.invoke(cli, ['--format', 'csv', '--output', outfile,
+                                     'stream', infile])
+        assert result.exit_code == 0
+        assert result.output == 'Found 1 tables\n'
