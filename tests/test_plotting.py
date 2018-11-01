837a657b04fc17b7d975d5e5c2234f89cdf13bb4
Vinayak Mehta <vmehta94@gmail.com>
2018-11-01 14:27:18 +0530

    Add tolerance

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -11,35 +11,40 @@ testdir = os.path.dirname(os.path.abspath(__file__))
 testdir = os.path.join(testdir, "files")
 
 
-@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots")
+@pytest.mark.mpl_image_compare(
+    baseline_dir="files/baseline_plots", tolerance=15)
 def test_text_plot():
     filename = os.path.join(testdir, "foo.pdf")
     tables = camelot.read_pdf(filename)
     return plot_text(tables[0]._text)
 
 
-@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots")
+@pytest.mark.mpl_image_compare(
+    baseline_dir="files/baseline_plots", tolerance=15)
 def test_contour_plot():
     filename = os.path.join(testdir, "foo.pdf")
     tables = camelot.read_pdf(filename)
     return plot_contour(tables[0]._image)
 
 
-@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots")
+@pytest.mark.mpl_image_compare(
+    baseline_dir="files/baseline_plots", tolerance=15)
 def test_table_plot():
     filename = os.path.join(testdir, "foo.pdf")
     tables = camelot.read_pdf(filename)
     return plot_table(tables[0])
 
 
-@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots")
+@pytest.mark.mpl_image_compare(
+    baseline_dir="files/baseline_plots", tolerance=15)
 def test_line_plot():
     filename = os.path.join(testdir, "foo.pdf")
     tables = camelot.read_pdf(filename)
     return plot_line(tables[0]._segments)
 
 
-@pytest.mark.mpl_image_compare(baseline_dir="files/baseline_plots")
+@pytest.mark.mpl_image_compare(
+    baseline_dir="files/baseline_plots", tolerance=15)
 def test_joint_plot():
     filename = os.path.join(testdir, "foo.pdf")
     tables = camelot.read_pdf(filename)
