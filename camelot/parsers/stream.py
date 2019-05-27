de3281c1b67b80d21a3079566d8e16c07cfb16ff
Vinayak Mehta <vmehta94@gmail.com>
2019-05-27 22:18:23 +0530

    Add test

--- camelot/parsers/stream.py
+++ camelot/parsers/stream.py
@@ -287,6 +287,10 @@ class Stream(BaseParser):
                 hor_text = []
                 for region in self.table_regions:
                     x1, y1, x2, y2 = region.split(",")
+                    x1 = float(x1)
+                    y1 = float(y1)
+                    x2 = float(x2)
+                    y2 = float(y2)
                     region_text = text_in_bbox((x1, y2, x2, y1), self.horizontal_text)
                     hor_text.extend(region_text)
             # find tables based on nurminen's detection algorithm
