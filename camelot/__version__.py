2f2314fc0f5931195a5d1923dabdc5358358014c
KOLANICH <kolan_n@mail.ru>
2019-07-15 20:36:55 +0300

    Moved most of metadata into setup.cfg

--- camelot/__version__.py
+++ camelot/__version__.py
@@ -13,11 +13,4 @@ def generate_version(version, prerelease=None, revision=None):
         version_parts.append(".{}".format(revision))
     return "".join(version_parts)
 
-
-__title__ = "camelot-py"
-__description__ = "PDF Table Extraction for Humans."
-__url__ = "http://camelot-py.readthedocs.io/"
 __version__ = generate_version(VERSION, prerelease=PRERELEASE, revision=REVISION)
-__author__ = "Vinayak Mehta"
-__author_email__ = "vmehta94@gmail.com"
-__license__ = "MIT License"
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
+from setuptools import setup
 import os
-from setuptools import find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -9,19 +8,7 @@ about = {}
 with open(os.path.join(here, 'camelot', '__version__.py'), 'r') as f:
     exec(f.read(), about)
 
-with open('README.md', 'r') as f:
-    readme = f.read()
-
-
-requires = [
-    'chardet>=3.0.4',
-    'click>=6.7',
-    'numpy>=1.13.3',
-    'openpyxl>=2.5.8',
-    'pandas>=0.23.4',
-    'pdfminer.six>=20170720',
-    'PyPDF2>=1.26.0'
-]
+__version__ = about["__version__"]
 
 cv_requires = [
     'opencv-python>=3.4.2.17'
@@ -43,47 +30,13 @@ dev_requires = [
 all_requires = cv_requires + plot_requires
 dev_requires = dev_requires + all_requires
 
-
-def setup_package():
-    metadata = dict(name=about['__title__'],
-                    version=about['__version__'],
-                    description=about['__description__'],
-                    long_description=readme,
-                    long_description_content_type="text/markdown",
-                    url=about['__url__'],
-                    author=about['__author__'],
-                    author_email=about['__author_email__'],
-                    license=about['__license__'],
-                    packages=find_packages(exclude=('tests',)),
-                    install_requires=requires,
-                    extras_require={
-                        'all': all_requires,
-                        'cv': cv_requires,
-                        'dev': dev_requires,
-                        'plot': plot_requires
-                    },
-                    entry_points={
-                        'console_scripts': [
-                            'camelot = camelot.cli:cli',
-                        ],
-                    },
-                    classifiers=[
-                        # Trove classifiers
-                        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-                        'License :: OSI Approved :: MIT License',
-                        'Programming Language :: Python :: 2.7',
-                        'Programming Language :: Python :: 3.5',
-                        'Programming Language :: Python :: 3.6',
-                        'Programming Language :: Python :: 3.7'
-                    ])
-
-    try:
-        from setuptools import setup
-    except ImportError:
-        from distutils.core import setup
-
-    setup(**metadata)
-
-
-if __name__ == '__main__':
-    setup_package()
+if __name__ == "__main__":
+    setup(
+        extras_require={
+            'all': all_requires,
+            'cv': cv_requires,
+            'dev': dev_requires,
+            'plot': plot_requires
+        },
+        use_scm_version=True
+    )
